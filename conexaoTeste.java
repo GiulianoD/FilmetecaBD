9d56e8cec2eb3d4dbb340762aae9dfaf55a1be55
Giuliano de Paulo Demoner <42439057+GiulianoD@users.noreply.github.com>
2022-05-25 10:51:32 -0300

    alterando estrutura geral do código
    alterando o código para como é sugerido pelo Heroku Dev Center
    *precisa alterar a database URL

--- conexaoTeste.java
+++ conexaoTeste.java
@@ -3,25 +3,34 @@ package com.api.filmeteca.bd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class Conexao {
-  public static void main(String[] args) {
-    String jdbcURL = "ec2-54-86-224-85.compute-1.amazonaws.com";
-    String usuario = "obfotxtfenewnb";
-    String senha = "d423f724b4c3d1d15bbc5d59bce5e59fc10338dc3f6678b7afeff3c3fb524b45";
+public class conexao {
 
-    try{
-      Connection conexao = DriverManager.getConnection(jdbcURL, usuario, senha);
-      /*Statement statement = conexao.createStatement();*/
-
-      System.out.println("Conectou");
-      conexao.close();
-
-    }catch(SQLException e){
-      System.out.println("Nao conectou");
-      e.printStackTrace();
-      
-    }
-  }
+	private static Connection conectar() throws URISyntaxException, SQLException{
+		URI dbURI= new URI (System.getenv("ec2-54-86-224-85.compute-1.amazonaws.com")); //DATABASE_URL
+		
+		String usuario = dbURI.getUserInfo().split(":")[0];
+		String senha = dbURI.getUserInfo().split(":")[1];
+		String dbURL = "jdbc:postgresql://" + dbURI.getHost() + ';' + dbURI.getPort() + dbURI.getPath() + "?sslmode=require";
+		
+		return DriverManager.getConnection(dbURL, usuario, senha);
+	}
+	
+	public static void main (String[] args){
+		
+		try {
+			Connection conexao = conectar();
+			System.out.println("Banco de Dados conectado com sucesso.");
+			
+			conexao.close();
+		} catch (URISyntaxException e) {
+			System.out.println("Error ao conectar com o bando de dados.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Error ao conectar com o bando de dados.");
+			e.printStackTrace();
+		}
+	}
 }
