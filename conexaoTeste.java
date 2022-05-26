3581ce98f821f6a1e5b2ecf744df7a634700c892
Giuliano de Paulo Demoner <42439057+GiulianoD@users.noreply.github.com>
2022-05-26 16:01:20 -0300

    pequena alteração
    ainda sem conseguir conectar.

--- conexaoTeste.java
+++ conexaoTeste.java
@@ -1,4 +1,4 @@
-package com.api.filmeteca.bd;
+package teste;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,30 +6,30 @@ import java.sql.SQLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class conexao {
-
-	private static Connection conectar() throws URISyntaxException, SQLException{
-		URI dbURI= new URI (System.getenv("ec2-54-86-224-85.compute-1.amazonaws.com")); //DATABASE_URL
+public class Teste {
+	private static final String URI_BD = "postgres://uaoqajtypqbpui:2bd8e016b21b8fdb6b2e716a55af85ebac9e421c038c52835bf64a8720150cdb@ec2-54-165-90-230.compute-1.amazonaws.com:5432/d4picedhjsu894";
+	
+	private static Connection conectar(String uri) throws URISyntaxException, SQLException{
+		
+		URI dbURI= new URI (System.getenv(uri));
 		
 		String usuario = dbURI.getUserInfo().split(":")[0];
 		String senha = dbURI.getUserInfo().split(":")[1];
-		String dbURL = "jdbc:postgresql://" + dbURI.getHost() + ';' + dbURI.getPort() + dbURI.getPath() + "?sslmode=require";
+		String dbURL = "jdbc:postgresql://" + dbURI.getHost() + ':' + dbURI.getPort() + dbURI.getPath() + "?sslmode=require";
 		
 		return DriverManager.getConnection(dbURL, usuario, senha);
 	}
 	
 	public static void main (String[] args){
-		
+	
 		try {
-			Connection conexao = conectar();
-			System.out.println("Banco de Dados conectado com sucesso.");
+			Connection conexao = conectar(URI_BD);
+			System.out.println("DBserver succesfully connected.");
 			
 			conexao.close();
 		} catch (URISyntaxException e) {
-			System.out.println("Error ao conectar com o bando de dados.");
 			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println("Error ao conectar com o bando de dados.");
 			e.printStackTrace();
 		}
 	}
